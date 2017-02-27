dadc75542f2101c0971c505178ae0379ec268125
Kazimieras Senvaitis <kazimieras.senvaitis@gmail.com>
2017-02-27 13:44:57 +0200

    working version

---
+++ Vehicle Alarm/Vehicle Alarm/ScriptTutorial.cs
@@ -0,0 +1,29 @@
+﻿using GTA;
+using System;
+using System.Windows.Forms;
+
+public class ScriptTutorial : Script
+{
+    public ScriptTutorial()
+    {
+        Tick += OnTick;
+        KeyDown += OnKeyDown;
+        KeyUp += OnKeyUp;
+
+        Interval = 10; // ms
+    }
+
+    void OnTick(object sender, EventArgs e)
+    {
+
+    }
+
+    void OnKeyDown(object sender, KeyEventArgs e)
+    {
+
+    }
+    void OnKeyUp(object sender, KeyEventArgs e)
+    {
+
+    }
+}
